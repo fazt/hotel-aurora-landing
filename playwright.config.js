6058bd5cdb2b92fe4804653cc5c3eef7b255cda1
Fazt <fazt@faztweb.com>
2026-08-16 23:02:13 -0500

    Add playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,16 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30_000,
+  use: {
+    baseURL: 'http://127.0.0.1:8765',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
